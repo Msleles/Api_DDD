d245ac12834508999e577ba6d53b4bc1ea57be63
= <=>
2021-12-21 00:18:03 -0300

    First Commit

---
+++ ApiTeste/User.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Domain.Entities;
+using System;
+using Xunit;
+
+namespace ApiTeste
+{
+   public class User
+    {
+
+        [Fact]
+
+        public void RetornaListaDeUsuarios()
+        {
+
+            //Arrange
+
+            var usuario = new UserEntity();
+            usuario.Id = Guid.NewGuid();
+            usuario.Nome = "Maycon Leles";
+            usuario.Email = "Maycon91leles@hotmail.com";
+            
+            //act
+
+
+            //Assert
+        }
+
+    }
+}

207236e5b1abc97e2f7f2bbd94e2cfc373338837
= <=>
2021-12-22 10:05:47 -0300

    criando controller Users

---
+++ ApiTeste/ControladorUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using Appication.Controllers;
+using Domain.Entities;
+using Domain.Services.user;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiTeste
+{
+   public class ControladorUsuarios
+    {
+
+        private readonly UsersController _userConroller;
+        protected readonly IUserService _userService;
+
+        public ControladorUsuarios(UsersController userConroller, IUserService userService)
+        {
+            _userConroller = userConroller;
+            _userService = userService;
+        }
+
+        [Fact]
+        public void GetUsuarios()
+        {
+            UserEntity user = new UserEntity();
+            {
+                user.Id = Guid.NewGuid();
+                user.Nome = "Maycon Leles";
+                user.Email = "Maycon91leles@hotmail.com";
+            }
+            var ActionResult = _userConroller.GetAll(_userService);
+
+            Assert.NotNull(ActionResult);           
+            Assert.True(ActionResult.IsCompletedSuccessfully);
+        }
+
+       
+    }
+}
--- Appication/Controllers/UsersController.cs
+++ Appication/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Services.user;
+﻿using Domain.Entities;
+using Domain.Services.user;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -18,16 +19,75 @@ namespace Appication.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             try
             {
                 return Ok(await service.GetAll());
             }
             catch (ArgumentException e)
             {
-
                 return StatusCode((int) HttpStatusCode.InternalServerError,e.Message);
             }
         }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromServices] IUserService service, UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.Post(user));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromServices] IUserService service, UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.Put(user));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
+        }
+
+        [HttpDelete("{Id}")]
+
+        public async Task<ActionResult> Delete([FromServices] IUserService service, Guid Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.Delete(Id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+
     }
 }
